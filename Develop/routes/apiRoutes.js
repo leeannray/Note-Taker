1b91ecd63a93053b372afc1d224bdb918b961ab0
Lee Ann Norman <leeann.norm@gmail.com>
2020-04-16 18:21:09 -0500

    minor changes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,18 @@
 
-const store = require("../db/notes");
+const data = require("../db/notes");
 const router = require("express").Router();
 
 
 // GET "/api/notes" responds with all notes from the database
 router.get("/notes", (req, res) => {
-  store
+  data
     .getNotes()
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
 
 router.post("/notes", (req, res) => {
-  store
+  data
     .addNote(req.body)
     .then(note => res.json(note))
     .catch(err => res.status(500).json(err));
@@ -21,7 +21,7 @@ router.post("/notes", (req, res) => {
 // DELETE "/api/notes" deletes note
 //id equal to req.params.id
 router.delete("/notes/:id", (req, res) => {
-  store
+  data
     .deleteNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch(err => res.status(500).json(err));
